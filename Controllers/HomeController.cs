72c81d66200fb6bf20a45a9a4c5bb6b00b794b93
Mylord2000 <44980460+Mylord2000@users.noreply.github.com>
2020-12-09 04:53:31 +0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtShop3.Models;
+using ArtShop3.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ArtShop3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IArtRepository _artRepository;
+
+        public HomeController(IArtRepository artRepository)
+        {
+            _artRepository = artRepository;
+        } 
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                ArtsOfTheWeek = _artRepository.ArtsOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
